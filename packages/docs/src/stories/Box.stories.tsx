dc70b26eae29c537cc538cecbab6b3e18b12f686
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2023-02-16 14:42:13 -0300

    Added components Box and Text

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,13 @@
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
